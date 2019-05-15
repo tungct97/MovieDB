02d0c4c90379d379eef13f431c9023e75e645c47
nguyen.dang.tung <FRAMGIA\nguyen.dang.tung@i120844-pc.framgia.com>
2019-05-15 13:48:46 +0700

    fix_comment

--- app/src/main/java/com/example/moviedb/ui/screen/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/moviedb/ui/screen/detail/DetailViewModel.kt
@@ -5,17 +5,9 @@ import com.example.moviedb.data.model.Movie
 import com.example.moviedb.ui.base.BaseViewModel
 
 class DetailViewModel : BaseViewModel() {
-    val pathImage: MutableLiveData<String> = MutableLiveData()
-    val releaseDate: MutableLiveData<String> = MutableLiveData()
-    val voteCount: MutableLiveData<Int> = MutableLiveData()
-    val overview: MutableLiveData<String> = MutableLiveData()
-    val title: MutableLiveData<String> = MutableLiveData()
+    var movies: MutableLiveData<Movie> = MutableLiveData()
 
     fun loadData(movie: Movie) {
-        pathImage.value = movie.backdropPath
-        releaseDate.value = movie.releaseDate
-        voteCount.value = movie.voteCount
-        overview.value = movie.overview
-        title.value = movie.title
+        movies.value = movie
     }
 }
